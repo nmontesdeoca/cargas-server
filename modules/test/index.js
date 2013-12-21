bc77172ded953dd56d65bddf649c28e8f06a2d3c
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2013-12-21 20:41:43 -0200

    deleting some files..

--- modules/test/index.js
+++
@@ -1,8 +0,0 @@
-var koa = require('koa'),
-    app = module.exports = koa(),
-    render = require('co-views')(__dirname + '/views', { ext: 'ejs' }),
-    route = require('koa-route');
-
-app.use(route.get('/', function * () {
-    this.body = yield render('index', { title : 'mi new title pelao' });
-}));
