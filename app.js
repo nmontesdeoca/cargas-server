6e14e161f0cb2b81008f5231f7e57f663690ea24
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2013-12-21 15:02:46 -0200

    reverting all in order to keep working with koa js (next-generation framework YEAH)

--- app.js
+++ app.js
@@ -1,148 +1,39 @@
 var express = require('express'),
     fs = require('fs'),
     app = express(),
-    http = require('http'),
-    path = require('path'),
-    routes,
+    utils = require('./lib/utils'),
     mongoose = require('mongoose'),
-    passport = require('passport'),
-    models_path = __dirname + '/models',
-    ejs_filters_path = __dirname + '/views/filters',
-    
-    GoogleStrategy = require('passport-google').Strategy,
-    User,
-    
-    ensureAuthorization = function (request, response, next) {
-        if (request.isAuthenticated()) {
-            return next();
-        }
-        return response.redirect('/login');
-    },
+    passport = require('passport');
 
-    requireJSFile = function (path, file) {
-        if (~file.indexOf('.js')) {
-            require(path + '/' + file);
-        }
-    },
 
-    requirePath = function (path) {
-        fs.readdirSync(path).forEach(requireJSFile.bind(requireJSFile, path));
-    };
+/**
+ * configure application
+ */
+require('./config/express')(app, express, passport);
 
-app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.compress());
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('car_gas_secret_key'));
-    app.use(express.session());
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
-    app.use(require('less-middleware')({ src: __dirname + '/public' }));
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.set('db', process.env.db || require('./config').db[app.get('env')]);
-    app.set('domain', process.env.domain || require('./config').domain);
-});
+/**
+ * configure passport
+ */
+require('./config/passport')(passport);
 
-app.configure('development', function () {
-    app.use(express.errorHandler());
-});
+/**
+ * bootstrap models
+ */
+utils.requirePath(__dirname + '/models');
 
-// bootstrap models
-requirePath(models_path);
+/**
+ * bootstrap ejs filters
+ */
+utils.requirePath(__dirname + '/views/filters');
 
-// bootstrap ejs filters
-requirePath(ejs_filters_path);
-
-// bootstrap db connection
+/**
+ * bootstrap db connection
+ */
 mongoose.connect(app.get('db'));
-mongoose.connection.on('error', console.error.bind(console, 'DB CONNECTION ERROR'));
-mongoose.connection.once('open', console.log.bind(console, 'DB CONNECTION OK'));
+mongoose.connection.on('error', console.error.bind(console, 'db connection error'));
+mongoose.connection.once('open', console.log.bind(console, 'db connection ok'));
 
-routes = {
-    index: require('./routes'),
-    user: require('./routes/user'),
-    refuel: require('./routes/refuel')
-};
 
-User = mongoose.model('User');
 
-passport.serializeUser(User.serialize.bind(User));
-passport.deserializeUser(User.deserialize.bind(User));
-
-passport.use(new GoogleStrategy({
-    returnURL: app.get('domain') + 'auth/google/return',
-    realm: app.get('domain')
-},
-function(identifier, profile, done) {
-    process.nextTick(function () {
-        User.findOne({ email: profile.emails[0].value }, function (error, user) {
-            var newUser;
-            if (!user) {
-                newUser = new User();
-                newUser.set({
-                    email: profile.emails[0].value,
-                    first_name: profile.name.givenName,
-                    last_name: profile.name.familyName
-                });
-                
-                newUser.save(function (error) {
-                    return User.deserialize(profile.emails[0].value, done);
-                });
-            } else {
-                return User.deserialize(profile.emails[0].value, done);
-            }
-        });
-    });
-}));
-
-app.get('/auth/google', passport.authenticate('google'));
-app.get('/auth/google/return', passport.authenticate('google', {
-    successRedirect: '/refuels',
-    failureRedirect: '/login'
-}));
-
-app.get('/', routes.index.index);
-app.get('/login', routes.user.login);
-app.get('/logout', routes.user.logout);
-app.get('/refuels', ensureAuthorization, routes.refuel.list);
-app.all('/refuels/create', ensureAuthorization, routes.refuel.create);
-
-/*
-// Only to create or set the fuel prices
-app.get('/fuels', function (request, response) {
-    var Fuel = mongoose.model('Fuel'),
-        premium = new Fuel(),
-        gasoil = new Fuel(),
-        super95 = new Fuel();
-
-    premium.set({
-        name: 'Premium 97 SP',
-        cost: 42.10
-    });
-    premium.save();
-
-    gasoil.set({
-        name: 'Gasoil',
-        cost: 38.70
-    });
-    gasoil.save();
-
-    super95.set({
-        name: 'Super 95 SP',
-        cost: 40.60
-    });
-    super95.save();
-
-    response.send('Combustibles actualizados');
-});
-*/
-
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('try the application from: http://localhost:3000');
-});
+app.listen(app.get('port'));
+console.log('try the application from: http://localhost:3000');
--- koa.js
+++ koa.js
@@ -1,42 +1,40 @@
 var koa = require('koa'),
-    app = koa(),
-    views = require('co-views'),
-    logger = require('koa-logger'),
-    route = require('koa-route'),
-    render = views(__dirname + '/views', { ext: 'ejs' }),
-    serve = require('koa-static'),
-    passport = require('passport');
+    app = koa();
 
 // x-response-time
-app.use(function *(next){
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    this.set('X-Response-Time', ms + 'ms');
-});
+app.use(require('koa-response-time')());
 
 // logger
-app.use(logger());
+app.use(require('koa-logger')());
 
+// static files
+app.use(require('koa-static')(__dirname + '/public'));
+
+// configuring routes
+app.use(require('./config/routes')(app));
+
+// server start listening
+app.listen(3000);
+console.log('listening on port 3000');
+
+/**
+ * EXAMPLES
+ *
 // response
-app.use(route.get('/', function *() {
+app.use(route.get('/', function * () {
     this.body = yield render('user/login', { title: 'Hello World' });
 }));
 
 // body parsing
-app.use(function *(next){
-  if ('POST' != this.method) return yield next;
-  var body = yield parse(this, { limit: '1kb' });
-  if (!body.name) this.throw(400, '.name required');
-  this.body = { name: body.name.toUpperCase() };
+app.use(function * (next) {
+    if ('POST' !== this.method) {
+        return yield next;
+    }
+    var body = yield parse(this, { limit: '1kb' });
+    if (!body.name) {
+        this.throw(400, '.name required');
+    }
+    this.body = { name: body.name.toUpperCase() };
 });
 
-app.use(serve(__dirname + '/public'));
-
-app.use(function *(next) {
-    passport.initialize();
-    passport.session();
-    yield next;
-});
-
-app.listen(3000);
+*/
