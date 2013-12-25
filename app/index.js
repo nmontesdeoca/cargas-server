0a2d5301eb67a3daefbe3a227cf506ea864f4000
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2013-12-24 22:16:57 -0200

    fix config

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     app = module.exports = express(),
-    utils = require('../lib/utils');
+    utils = require('../lib/utils'),
+    domain,
+    config;
 
 /**
  * configure the express application and connect to database
@@ -15,7 +17,25 @@ utils.requireModules(__dirname + '/modules', app);
 /**
  * bootstrap passport configuration
  */
-require('../config/passport')(require('passport'), require('../config/config')(app));
+domain = app.get('domain');
+config = {
+    facebook: { callbackURL: domain + '/auth/facebook/callback' },
+    twitter: { callbackURL: domain + '/auth/twitter/callback' },
+    google: { callbackURL: domain + '/auth/google/callback' }
+};
+if (process.env.CONFIGURED) {
+    config.facebook.clientID = process.env.FACEBOOK_CLIENT_ID;
+    config.facebook.clientSecret = process.env.FACEBOOK_CLIENT_SECRET;
+
+    config.twitter.clientID = process.env.TWITTER_CLIENT_ID;
+    config.twitter.clientSecret = process.env.TWITTER_CLIENT_SECRET;
+
+    config.google.clientID = process.env.GOOGLE_CLIENT_ID;
+    config.google.clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+} else {
+    config = require('../config/config')(config);
+}
+require('../config/passport')(require('passport'), config);
 
 /**
  * bootstrap ejs filters
