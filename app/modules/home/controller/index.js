cf8b6c8a18553421356f66a48671ab3807effbf6
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2013-12-24 22:19:23 -0200

    fix

--- app/modules/home/controller/index.js
+++ app/modules/home/controller/index.js
@@ -4,6 +4,6 @@ module.exports = {
          * por ahora no me interesa que haya una home page, asi que redirijo a la pagina de refuels
          * que me parece mas productivo para un usuario, para ya ponerse a ingresar los datos
          */
-        response.redirect('/refuel');
+        response.redirect('/refuels');
     }
 };
