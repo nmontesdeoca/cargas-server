b2f572b370ef468a566ce3a10221205fb4c36bd7
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2014-01-22 19:41:36 -0200

    @neggro changes

--- app/middlewares/authorization.js
+++ app/middlewares/authorization.js
@@ -1,5 +1,7 @@
+var passport = require('passport');
+
 /**
- * middleware to check if the user has the right acces
+ * middleware to check if the user has the right access
  * @param  {Object}   request  express request
  * @param  {Object}   response express response
  * @param  {Function} next     express next function
@@ -10,4 +12,32 @@ exports.requiresLogin = function (request, response, next) {
         return response.send(401);
     }
     next.apply(this);
+};
+
+/**
+ * middleware to login local users (stored in our db)
+ * @param  {Object}   request
+ * @param  {Object}   response
+ * @param  {Function} next
+ * @return {void}
+ */
+exports.localLogin = function (request, response, next) {
+    passport.authenticate('local', function (error, user, info) {
+        if (error) {
+            return next(error);
+        }
+        if (!user) {
+            return response.send({
+                errorMessage: info.message
+            });
+        }
+        request.logIn(user, function (error) {
+            if (error) {
+                return next(error);
+            }
+            return response.send({
+                user: user
+            });
+        });
+    })(request, response, next);
 };
