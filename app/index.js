48db3fad24c393abe73b156a4edd285d6f41a25f
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2014-02-05 21:46:10 -0200

    added handler for OPTIONS request sent by Angular

--- app/index.js
+++ app/index.js
@@ -2,6 +2,23 @@ var express = require('express'),
     app = module.exports = express(),
     utils = require('../lib/utils');
 
+app.use(function (request, response, next) {
+
+    response.set({
+        'Access-Control-Allow-Origin': '*'
+    });
+
+    if (request.method === 'OPTIONS') {
+        response.set({
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With'
+        });
+        response.send(200);
+    } else {
+        next.apply(this);
+    }
+});
+
 /**
  * configure the express application and connect to database
  */
