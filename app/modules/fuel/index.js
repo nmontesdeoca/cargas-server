5febc2f1e12f5830d00a8e7033dafa607c1adbd5
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2014-02-12 19:40:04 -0200

    changed bodyParser to two middlewares, improve

--- app/modules/fuel/index.js
+++ app/modules/fuel/index.js
@@ -2,7 +2,9 @@ var applicationFactory = require('../../../lib/application/factory'),
     app = module.exports = applicationFactory.newApplication(function (app, express) {
         app.configure(function () {
             app.set('name', 'fuel');
-            app.use(express.bodyParser());
+            // app.use(express.bodyParser());
+            app.use(express.json());
+            app.use(express.urlencoded());
             app.use(express.methodOverride());
             app.use(app.router);
         });
